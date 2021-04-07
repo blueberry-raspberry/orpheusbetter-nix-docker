e7de7b6374bc230823886543c8bb6190280178fa
Blueberry Raspberry <81907694+blueberry-raspberry@users.noreply.github.com>
2021-04-07 15:40:03 +0200

    bump version

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 let
   nixpkgsRev = "c0e881852006b132236cbf0301bd1939bb50867e";
-  imageVersion = "6";
+  imageVersion = "7";
 
   nixpkgsURL = "https://github.com/NixOS/Nixpkgs/archive/${nixpkgsRev}.tar.gz";
   pkgsDefault =
